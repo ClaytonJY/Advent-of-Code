1ced005a139958b5d9591bfd5a1f84f1babb049f
Clayton Yochum <claytonjy@gmail.com>
2021-12-03 00:44:38 -0500

    Implement Runner class and use in existing daily modules.

--- 2021/python/day_1.py
+++ 2021/python/day_1.py
@@ -1,36 +1,34 @@
 from pathlib import Path
 from typing import List
 
-
-def part1(input):
-    last = None
-    increases = 0
-    for line in input:
-        current = int(line)
-        if last and current > last:
-            increases += 1
-        last = current
-
-    return increases
+from utils import Runner
 
 
-def part2(input):
-    numbers = [int(x) for x in input]
+class Day1(Runner):
+    def part1(self):
+        last = None
+        increases = 0
+        for line in self.lines:
+            current = int(line)
+            if last and current > last:
+                increases += 1
+            last = current
 
-    past_sum = None
-    increases = 0
-    for a, b, c in zip(numbers[:-2], numbers[1:-1], numbers[2:]):
-        sum = a + b + c
-        if past_sum and sum > past_sum:
-            increases += 1
-        past_sum = sum
+        return increases
 
-    return increases
+    def part2(self):
+        numbers = [int(x) for x in self.lines]
+
+        past_sum = None
+        increases = 0
+        for a, b, c in zip(numbers[:-2], numbers[1:-1], numbers[2:]):
+            sum = a + b + c
+            if past_sum and sum > past_sum:
+                increases += 1
+            past_sum = sum
+
+        return increases
 
 
 if __name__ == "__main__":
-    stem: str = Path(__file__).stem
-    input_file = Path("inputs") / (stem + ".txt")
-    input: List[str] = input_file.read_text().splitlines()
-
-    print(part1(input), part2(input))
+    Day1(__file__).run()
--- 2021/python/day_2.py
+++ 2021/python/day_2.py
@@ -1,41 +1,39 @@
 from pathlib import Path
 from typing import List
 
-
-def part1(input):
-    horizontal, vertical = 0, 0
-    for line in input:
-        direction, amount = line.split(" ")
-        amount = int(amount)
-        if direction == "forward":
-            horizontal += amount
-        elif direction == "up":
-            vertical -= amount
-        elif direction == "down":
-            vertical += amount
-
-    return horizontal * vertical
+from utils import Runner
 
 
-def part2(input):
-    horizontal, depth, aim = 0, 0, 0
-    for line in input:
-        direction, amount = line.split(" ")
-        amount = int(amount)
-        if direction == "forward":
-            horizontal += amount
-            depth += amount * aim
-        elif direction == "down":
-            aim += amount
-        elif direction == "up":
-            aim -= amount
+class Day2(Runner):
+    def part1(self) -> int:
+        horizontal, vertical = 0, 0
+        for line in self.lines:
+            direction, amount = line.split(" ")
+            amount = int(amount)
+            if direction == "forward":
+                horizontal += amount
+            elif direction == "up":
+                vertical -= amount
+            elif direction == "down":
+                vertical += amount
 
-    return horizontal * depth
+        return horizontal * vertical
+
+    def part2(self) -> int:
+        horizontal, depth, aim = 0, 0, 0
+        for line in self.lines:
+            direction, amount = line.split(" ")
+            amount = int(amount)
+            if direction == "forward":
+                horizontal += amount
+                depth += amount * aim
+            elif direction == "down":
+                aim += amount
+            elif direction == "up":
+                aim -= amount
+
+        return horizontal * depth
 
 
 if __name__ == "__main__":
-    stem: str = Path(__file__).stem
-    input_file = Path("inputs") / (stem + ".txt")
-    input: List[str] = input_file.read_text().splitlines()
-
-    print(part1(input), part2(input))
+    Day2(__file__).run()
