ff33e4081e1c5d8efa2a5a935bba2e5037242b15
Clayton Yochum <claytonjy@gmail.com>
2021-12-04 20:25:11 -0500

    Minor cleanup to day 4.

--- 2021/python/day_4.py
+++ 2021/python/day_4.py
@@ -1,6 +1,6 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from pathlib import Path
-from typing import List, Optional, Tuple
+from typing import Dict, List
 
 import numpy as np
 from numpy.typing import NDArray
@@ -10,14 +10,14 @@ from numpy.typing import NDArray
 class Board:
     board: NDArray[np.int_]
 
-    def play(self, numbers: List[int]):
+    def play(self, numbers: List[int]) -> Dict[str, int]:
         bingo_sum = self.board.shape[0]
         for i, _ in enumerate(numbers):
             subset = numbers[: (i + 1)]
             mask = np.isin(self.board, subset)
             if bingo_sum in np.concatenate([mask.sum(0), mask.sum(1)]):
                 score = self.board[~mask].sum() * numbers[i]
-                return score, i
+                return {"score": score, "turns": i}
 
 
 @dataclass
@@ -25,37 +25,30 @@ class Bingo:
     numbers: List[int]
     boards: List[Board]
 
+    def play(self) -> List[Dict[str, int]]:
+
+        results = [board.play(self.numbers) for board in self.boards]
+        return sorted(results, key=lambda x: x["turns"])
+
     @classmethod
     def from_file(cls, path: Path):
         numbers_txt, *boards_txt = path.read_text().split("\n\n")
 
         numbers = [int(x) for x in numbers_txt.split(",")]
-        boards = [
-            Board(
-                np.array(
-                    [
-                        [int(x) for x in line.split()]
-                        for line in board.strip().split("\n")
-                    ]
-                )
-            )
-            for board in boards_txt
-        ]
+        boards = []
+        for board_txt in boards_txt:
+            lines: List[str] = board_txt.strip().splitlines()
+            rows: List[List[int]] = [[int(x) for x in line.split()] for line in lines]
+            board = Board(np.array(rows))
+            boards.append(board)
 
         return cls(numbers, boards)
-
-    def play(self):
-
-        results: List[Tuple[int, int]] = [
-            board.play(self.numbers) for board in self.boards
-        ]
-        return sorted(results, key=lambda x: x[1])
 
 
 if __name__ == "__main__":
     input = Path("inputs/day_4.txt")
     game = Bingo.from_file(input)
     scores = game.play()
-    print(scores)
-    print(f"part 1: {scores[0][0]}")
-    print(f"part 2: {scores[-1][0]}")
+
+    print(f"part 1: {scores[0]['score']}")
+    print(f"part 2: {scores[-1]['score']}")
